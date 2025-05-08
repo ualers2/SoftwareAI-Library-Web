e4b130e36466b85fc6fc756438c001375f45b8c4
UALERSON <mediacutsstudio@gmail.com>
2025-05-07 21:27:56 -0300

    Add Integration.py

---
+++ Agents/Code/DevOps/DockerBuild/Integration.py
@@ -0,0 +1,76 @@
+from agents import Agent, handoff, RunContextWrapper
+import requests
+from dotenv import load_dotenv, find_dotenv
+import os
+from agents.extensions.handoff_prompt import RECOMMENDED_PROMPT_PREFIX
+from pydantic import BaseModel
+from modules.modules import *
+
+
+
+class FrontEndData(BaseModel):
+    FrontEndContent: str
+
+async def on_handoff(ctx: RunContextWrapper[dict], input_data: FrontEndData):
+    print(f"CodeDockerBuildAgent: {input_data.FrontEndContent}")
+    # response = requests.post("http://localhost:5000/agent/refund", json={"input": input_data.reason})
+    # reply = response.json().get("reply")
+              
+def CodeDockerBuildAgent(session_id, appcompany):
+   
+    path_ProjectWeb = "/app/LocalProject"
+    os.chdir(path_ProjectWeb)
+    path_html = f"{path_ProjectWeb}/templates"
+    path_js = f"{path_ProjectWeb}/static/js"
+    path_css = f"{path_ProjectWeb}/static/css"
+    doc_md = f"{path_ProjectWeb}/doc_md"
+
+    githubtoken = os.getenv("github_username_Ualers")
+    repo_owner = os.getenv("companyname")
+
+
+   
+    dockerbuild = '''
+import subprocess
+import os
+
+os.chdir(os.path.join(os.path.dirname(__file__)))
+# Adiciona o caminho do Docker Compose
+os.environ["PATH"] += r";C:\Program Files\Docker\Docker\resources\bin"
+
+def executar_comando(comando):
+    """Executa um comando sem abrir um novo terminal (funciona dentro do contêiner)."""
+    subprocess.run(comando, shell=True)
+
+
+executar_comando("docker-compose up --build")
+
+
+    '''
+
+
+    name_agent, model_agent, instruction_original, tools_agent  = get_settings_agents(path_metadata = os.path.join(os.path.dirname(__file__), "metadata.json"))
+    instruction_formatado = format_instruction(instruction_original, locals())
+
+    Tools_Name_dict = Egetoolsv2(list(tools_agent))
+    agent = Agent(
+        name=name_agent,
+        instructions = f"""
+{RECOMMENDED_PROMPT_PREFIX}
+{instruction_formatado}
+        """
+        ,
+        model=str(model_agent),
+        tools=Tools_Name_dict,
+        # handoffs=[handoff_obj_CodeDocumentationModulesAgent],
+
+    )
+
+
+
+    handoff_obj = handoff(
+        agent=agent,
+        on_handoff=on_handoff,
+        input_type=FrontEndData,
+    )
+    return agent, handoff_obj
